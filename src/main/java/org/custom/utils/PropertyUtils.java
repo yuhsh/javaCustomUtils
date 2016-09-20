beb11e10b24a3a3638f510226204e15691944856
yuhaisheng <haisheng023@163.com>
2016-09-20 10:02:01 +0800

    PropertyUtils更新

--- src/main/java/org/custom/utils/PropertyUtils.java
+++ src/main/java/org/custom/utils/PropertyUtils.java
@@ -14,18 +14,36 @@ public final class PropertyUtils {
 
     public static String getPropertyValue(String _propertiesName, String key) {
         String value = "";
-        Properties prop = new Properties();
-        InputStream in = PropertyUtils.class
+        Properties properties = new Properties();
+        InputStream inputStream = PropertyUtils.class
                 .getResourceAsStream(_propertiesName);
+//         通过类加载器读取
+//        properties.load(new FileReader(_propertiesName));
+//        通过类加载器的路径来读取配置文件
+//        properties.load(PropertyUtils.class
+//                .getResourceAsStream(_propertiesName));
         try {
-            prop.load(in);
-            if (prop.getProperty(key) != null
-                    && !"".equals(prop.getProperty(key))) {
-                value = prop.getProperty(key).trim();
+            properties.load(inputStream);
+//            // 读取属性
+//            properties.getProperty(key);
+//            properties.getProperty(key, defaultValue);
+//            // 修改属性
+//            properties.setProperty(key, value);
+            if (properties.getProperty(key) != null
+                    && !"".equals(properties.getProperty(key))) {
+                value = properties.getProperty(key).trim();
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+                properties.clear();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
         return value;
     }
+    
 }
